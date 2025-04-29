f84f78a26bc4062997fb9675889f66333f8be929
20251411ytj <yoon_8540@gwnu.ac.kr>
2025-04-29 14:28:16 +0900

    제발되라

---
+++ Midterm2/Problem1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int PassOrFail(int score) {
+    if (score >= 50) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    int score = 72;
+    int result = PassOrFail(score);
+
+    if (result == 1) {
+        printf("재시험 없습니다!\n");
+    }
+    else {
+        printf("재시험 있습니다.\n");
+    }
+
+    return 0;
+}
---
+++ Midterm2/Problem2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int SS(int mp, int range) {
+    if (mp >= 50 && range > 10) {
+        printf("주인공 : 너는 점수를 획득했나?\n");
+    }
+    return 0;
+}
+
+int main() {
+    int mp = 150;
+    int range = 20;
+
+    SS(mp, range);
+
+    return 0;
+}
