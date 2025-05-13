6dd4917f5ae261b21c9703a6dae4d889b900c838
20251411ytj <yoon_8540@gwnu.ac.kr>
2025-05-13 12:47:44 +0900

    안녕 내 이름은 광식이야

---
+++ hi my name is gwang sik/hi my name is gwang sik/gwangsik.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 'G';
+    char b = 'W';
+    char c = 'A';
+    char d = 'N';
+    char e = 'G';
+    char f = ' ';
+    char g = 'S';
+    int h = 1;
+    char i = 'K';
+
+    printf("%c,%c,%c,%c,%c,%c,%c,%d,%c\n", a, b, c, d,e,f,g,h,i);
+
+    return 0;
+}
